8734c465bea09462bb7fb9dfbf29be540d4fb663
HardiB.Salih <30411248+HardiB-Salih@users.noreply.github.com>
2023-07-14 11:30:21 +0300

    Initial Commit

---
+++ Smack/SmackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmackApp.swift
+//  Smack
+//
+//  Created by HardiB.Salih on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct SmackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
